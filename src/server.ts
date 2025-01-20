c3d22ebde081abbdefa2599c251f3b80df36185c
Cristoffer Laner <cristofferrlaner@hotmail.com>
2025-01-20 12:38:21 -0300

    Downloaded dotenv and implemented PORT variable

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-
 import { Request, Response } from 'express'
 import { router } from "./routes/index"
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express()
-const port = 8089
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(router)
